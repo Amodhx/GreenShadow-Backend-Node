bbf3d0973e82efa36d638490e2a08ccefeedeb97
Amodhx <amxdhnanditha@gmail.com>
2025-01-27 20:51:51 +0530

    set routes in main router

--- src/routes/main.route.ts
+++ src/routes/main.route.ts
@@ -1,4 +1,10 @@
 import {Router} from "express";
+import CropRouter from "./crop.route";
+import EquipmentRouter from "./equipment.route";
+import FieldRouter from "./field.route";
+import LogRouter from "./log.route";
+import StaffRouter from "./staff.route";
+import VehicleRouter from "./vehicle.route";
 
 class MainRoute{
     router :Router
@@ -8,6 +14,12 @@ class MainRoute{
         this.initialRoutes();
     }
     initialRoutes(){
+        this.router.use('/crop',CropRouter.router)
+        this.router.use('/equipment',EquipmentRouter.router)
+        this.router.use('/field',FieldRouter.router)
+        this.router.use('/log',LogRouter.router)
+        this.router.use('/staff',StaffRouter.router)
+        this.router.use('/vehicle',VehicleRouter.router)
 
     }
 }
