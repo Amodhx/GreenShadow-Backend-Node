8443b004dc5e08a6098b3c6a8937e05b04f51e8f
Amodhx <amxdhnanditha@gmail.com>
2025-02-15 09:05:36 +0530

    done log crud

--- src/controller/log.controller.ts
+++ src/controller/log.controller.ts
@@ -1,17 +1,105 @@
 import {Request, Response} from "express";
+import fieldService from "../service/field.service";
+import {unwatchFile} from "node:fs";
+import LogModel from "../model/log.model";
+import logService from "../service/log.service";
+import equipmentService from "../service/equipment.service";
 
 class LogController{
-    async saveLog(req:Request,resp:Response){
-
+    async saveLog(req:Request,resp:Response):Promise<void>{
+        try {
+            if (!req.file){
+                resp.status(400).json({message : "NO FILE UPLOADED"})
+                return
+            }
+            const file = req.file;
+            const base64 = file.buffer.toString("base64")
+            const data = req.body;
+            if (!data.field_code_list){
+                data.field_code_list = []
+            }else {
+                data.field_code_list = data.field_code_list.split(',')
+            }
+            if (!data.staffs_list){
+                data.staffs_list = []
+            }else {
+                data.staffs_list = data.staffs_list.split(',')
+            }
+            if (!data.crops_list){
+                data.crops_list = [];
+            }else {
+                data.crops_list = data.crops_list.split(',')
+            }
+            const model = new LogModel(
+                data.log_id,
+                data.log_date,
+                data.log_details,
+                data.log_type,
+                base64,
+                data.field_code_list,
+                data.crops_list,
+                data.staffs_list
+            );
+            resp.status(201).send(await logService.saveLog(model))
+        }catch (err){
+            resp.status(500).send(err);
+        }
     }
-    async updateLog(req:Request,resp:Response){
-
+    async updateLog(req:Request,resp:Response):Promise<void>{
+        try {
+            if (!req.file){
+                resp.status(400).json({message : "NO FILE UPLOADED"})
+                return
+            }
+            const file = req.file;
+            const base64 = file.buffer.toString("base64")
+            const data = req.body;
+            if (!data.field_code_list){
+                data.field_code_list = []
+            }else {
+                data.field_code_list = data.field_code_list.split(',')
+            }
+            if (!data.staffs_list){
+                data.staffs_list = []
+            }else {
+                data.staffs_list = data.staffs_list.split(',')
+            }
+            if (!data.crops_list){
+                data.crops_list = [];
+            }else {
+                data.crops_list = data.crops_list.split(',')
+            }
+            const model = new LogModel(
+                data.log_id,
+                data.log_date,
+                data.log_details,
+                data.log_type,
+                base64,
+                data.field_code_list,
+                data.crops_list,
+                data.staffs_list
+            );
+            resp.status(201).send(await logService.updateLog(model))
+        }catch (err){
+            resp.status(500).send(err);
+        }
     }
     async deleteLog(req:Request,resp:Response){
-
+        try {
+            const  id = req.query['id'];
+            if (typeof id === "string") {
+                resp.status(201).send(await logService.deleteLog(id))
+            }
+        }catch (err){
+            resp.status(500).send(err);
+        }
     }
     async getAllLogs(req:Request,resp:Response){
-
+        try {
+            resp.status(201).send(await logService.getAllLogs())
+        }catch (err){
+            resp.status(500).send(err);
+        }
     }
 }
 const Log_controller = new LogController();
--- src/service/log.service.ts
+++ src/service/log.service.ts
@@ -1,18 +1,36 @@
 import LogModel from "../model/log.model";
+import logDao from "../dao/impl/log.dao";
 
 class LogService{
 
     async saveLog(logObj:LogModel){
-
+        try {
+            logObj.log_id = "LOG-1"
+            return await logDao.create(logObj);
+        }catch (err){
+            throw err;
+        }
     }
     async updateLog(logObj:LogModel){
-
+        try {
+            return await logDao.update(logObj);
+        }catch (err){
+            throw err;
+        }
     }
     async deleteLog(log_id:string){
-
+        try {
+            return await logDao.delete(log_id);
+        }catch (err){
+            throw err;
+        }
     }
     async getAllLogs(){
-
+        try {
+            return await logDao.findAll();
+        }catch (err){
+            throw err;
+        }
     }
 }
 const Log_service = new LogService();
