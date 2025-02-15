dd458990b852e1d4383833254f7854c323761542
Amodhx <amxdhnanditha@gmail.com>
2025-02-15 17:08:30 +0530

    added authentication and authorization to field route

--- src/routes/field.route.ts
+++ src/routes/field.route.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import Field_controller from "../controller/field.controller";
 import multer from "multer";
+import Authentication_Check from "../middleware/authentication";
+import Authorization_check from "../middleware/authorization";
 
 const storage = multer.memoryStorage(); // Store files in memory (you can change it to disk storage)
 const upload = multer({ storage });
@@ -11,10 +13,10 @@ class FieldRoute{
         this.initialRoutes();
     }
     initialRoutes(){
-        this.router.post('/saveField',upload.single('field_image'),Field_controller.saveField)
-        this.router.get('/getAllField',Field_controller.getAllFields)
-        this.router.patch('/updateField',upload.single('field_image'),Field_controller.updateField)
-        this.router.delete('/deleteField',Field_controller.deleteField)
+        this.router.post('/saveField',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','SCIENTIST']),upload.single('field_image'),Field_controller.saveField)
+        this.router.get('/getAllField',Authentication_Check.verifyToken,Field_controller.getAllFields)
+        this.router.patch('/updateField',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','SCIENTIST']),upload.single('field_image'),Field_controller.updateField)
+        this.router.delete('/deleteField',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','SCIENTIST']),Field_controller.deleteField)
     }
 }
 const FieldRouter = new FieldRoute();
