356793ce4f54c3aa64d144ff222aa1026c9cfcf5
Amodhx <amxdhnanditha@gmail.com>
2025-02-15 17:07:14 +0530

    added authentication and authorization to equipment route

--- src/routes/equipment.route.ts
+++ src/routes/equipment.route.ts
@@ -1,5 +1,7 @@
 import {Router} from "express";
 import Equipment_controller from "../controller/equipment.controller";
+import Authentication_Check from "../middleware/authentication";
+import Authorization_check from "../middleware/authorization";
 
 class EquipmentRoute{
     router:Router
@@ -8,10 +10,10 @@ class EquipmentRoute{
         this.initialRoutes();
     }
     initialRoutes(){
-        this.router.post('/saveEquipment',Equipment_controller.saveEquipment)
-        this.router.get('/getAllEquipments',Equipment_controller.getAllEquipments)
-        this.router.patch('/updateEquipment',Equipment_controller.updateEquipment)
-        this.router.delete('/deleteEquipment',Equipment_controller.deleteEquipment)
+        this.router.post('/saveEquipment',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Equipment_controller.saveEquipment)
+        this.router.get('/getAllEquipments',Authentication_Check.verifyToken,Equipment_controller.getAllEquipments)
+        this.router.patch('/updateEquipment',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Equipment_controller.updateEquipment)
+        this.router.delete('/deleteEquipment',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Equipment_controller.deleteEquipment)
     }
 }
 const EquipmentRouter = new EquipmentRoute();
