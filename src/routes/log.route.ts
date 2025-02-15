2310f0b01c9525671a442fb2e799b3403685637c
Amodhx <amxdhnanditha@gmail.com>
2025-02-15 17:09:36 +0530

    added authentication and authorization to log route

--- src/routes/log.route.ts
+++ src/routes/log.route.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import Log_controller from "../controller/log.controller";
 import multer from "multer";
+import Authentication_Check from "../middleware/authentication";
+import Authorization_check from "../middleware/authorization";
 
 const storage = multer.memoryStorage(); // Store files in memory (you can change it to disk storage)
 const upload = multer({ storage });
@@ -11,10 +13,10 @@ class LogRoute{
         this.initialRoutes();
     }
     initialRoutes(){
-        this.router.post('/saveLog',upload.single('observe_image'),Log_controller.saveLog)
-        this.router.get('/getAllLogs',Log_controller.getAllLogs)
-        this.router.patch('/updateLog',upload.single('observe_image'),Log_controller.updateLog)
-        this.router.delete('/deleteLog',Log_controller.deleteLog)
+        this.router.post('/saveLog',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','SCIENTIST']),upload.single('observe_image'),Log_controller.saveLog)
+        this.router.get('/getAllLogs',Authentication_Check.verifyToken,Log_controller.getAllLogs)
+        this.router.patch('/updateLog',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','SCIENTIST']),upload.single('observe_image'),Log_controller.updateLog)
+        this.router.delete('/deleteLog',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','SCIENTIST']),Log_controller.deleteLog)
     }
 }
 const LogRouter = new LogRoute();
