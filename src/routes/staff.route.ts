a5da328d61a466e95458d71661c62d260c5ad4db
Amodhx <amxdhnanditha@gmail.com>
2025-02-15 17:11:08 +0530

    added authentication and authorization to staff route

--- src/routes/staff.route.ts
+++ src/routes/staff.route.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import Crop_controller from "../controller/crop.controller";
 import Staff_controller from "../controller/staff.controller";
+import Authentication_Check from "../middleware/authentication";
+import Authorization_check from "../middleware/authorization";
 
 class StaffRoute{
     router:Router
@@ -9,10 +11,10 @@ class StaffRoute{
         this.initialRoutes();
     }
     initialRoutes(){
-        this.router.post('/saveStaff',Staff_controller.saveStaff)
-        this.router.get('/getAllStaffs',Staff_controller.getAllStaff)
-        this.router.patch('/updateStaff',Staff_controller.updateStaff)
-        this.router.delete('/deleteStaff',Staff_controller.deleteStaff)
+        this.router.post('/saveStaff',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Staff_controller.saveStaff)
+        this.router.get('/getAllStaffs',Authentication_Check.verifyToken,Staff_controller.getAllStaff)
+        this.router.patch('/updateStaff',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Staff_controller.updateStaff)
+        this.router.delete('/deleteStaff',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Staff_controller.deleteStaff)
     }
 }
 const StaffRouter = new StaffRoute();
