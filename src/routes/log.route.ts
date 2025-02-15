21a5cbdc2a74ea42c8bf2b605e0550d239cca910
Amodhx <amxdhnanditha@gmail.com>
2025-02-15 09:07:27 +0530

    issue fixed in log route

--- src/routes/log.route.ts
+++ src/routes/log.route.ts
@@ -1,6 +1,9 @@
 import {Router} from "express";
 import Log_controller from "../controller/log.controller";
+import multer from "multer";
 
+const storage = multer.memoryStorage(); // Store files in memory (you can change it to disk storage)
+const upload = multer({ storage });
 class LogRoute{
     router:Router
     constructor() {
@@ -8,9 +11,9 @@ class LogRoute{
         this.initialRoutes();
     }
     initialRoutes(){
-        this.router.post('/saveLog',Log_controller.saveLog)
+        this.router.post('/saveLog',upload.single('observe_image'),Log_controller.saveLog)
         this.router.get('/getAllLogs',Log_controller.getAllLogs)
-        this.router.patch('/updateLog',Log_controller.updateLog)
+        this.router.patch('/updateLog',upload.single('observe_image'),Log_controller.updateLog)
         this.router.delete('/deleteLog',Log_controller.deleteLog)
     }
 }
