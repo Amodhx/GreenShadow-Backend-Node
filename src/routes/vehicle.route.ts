7b5a53b35955ccdf821e8aee475cc0c826374db4
Amodhx <amxdhnanditha@gmail.com>
2025-02-15 17:12:18 +0530

    added authentication and authorization to vehicle route

--- src/routes/vehicle.route.ts
+++ src/routes/vehicle.route.ts
@@ -2,6 +2,8 @@ import {Router} from "express";
 import Crop_controller from "../controller/crop.controller";
 import Vehicle_controller from "../controller/vehicle.controller";
 import VehicleController from "../controller/vehicle.controller";
+import Authentication_Check from "../middleware/authentication";
+import Authorization_check from "../middleware/authorization";
 
 class VehicleRoute{
     router:Router
@@ -10,10 +12,10 @@ class VehicleRoute{
         this.initialRoutes();
     }
     initialRoutes(){
-        this.router.post('/saveVehicle',Vehicle_controller.saveVehicle)
-        this.router.get('/getAllVehicle',Vehicle_controller.getAllVehicles)
-        this.router.patch('/updateVehicle',Vehicle_controller.updateVehicle)
-        this.router.delete('/deleteVehicle',Vehicle_controller.deleteVehicle)
+        this.router.post('/saveVehicle',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Vehicle_controller.saveVehicle)
+        this.router.get('/getAllVehicle',Authentication_Check.verifyToken,Vehicle_controller.getAllVehicles)
+        this.router.patch('/updateVehicle',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Vehicle_controller.updateVehicle)
+        this.router.delete('/deleteVehicle',Authentication_Check.verifyToken,Authorization_check.checkRoles(['MANAGER','ADMINISTRATIVE']),Vehicle_controller.deleteVehicle)
     }
 }
 const VehicleRouter = new VehicleRoute();
