9dc380aac19bd08ea626b05f099c1c69acc94174
Amodhx <amxdhnanditha@gmail.com>
2025-02-19 11:54:09 +0530

    issue fixed in get all equipments

--- src/model/equipment.model.ts
+++ src/model/equipment.model.ts
@@ -1,14 +1,16 @@
+import {equipment_status} from "@prisma/client";
+
 class EquipmentModel{
     equipment_id:string
     equipment_name:string
     type:string
     count:string
-    status:EquipmentStatus
+    status:equipment_status
     staff_list:string[]
     field_list:string[]
 
 
-    constructor(equipment_id: string, equipment_name: string, type: string, count: string, status: EquipmentStatus, staff_id_list: string[], field_code_list: string[]) {
+    constructor(equipment_id: string, equipment_name: string, type: string, count: string, status: equipment_status, staff_id_list: string[], field_code_list: string[]) {
         this.equipment_id = equipment_id;
         this.equipment_name = equipment_name;
         this.type = type;
@@ -19,7 +21,3 @@ class EquipmentModel{
     }
 }
 export default EquipmentModel;
-
-export enum EquipmentStatus{
-    AVAILABLE,NOT_AVAILABLE
-}
--- src/service/equipment.service.ts
+++ src/service/equipment.service.ts
@@ -1,5 +1,8 @@
 import EquipmentModel from "../model/equipment.model";
 import equipmentDao from "../dao/impl/equipment.dao";
+import {EquipmentDto} from "../dto/equipment.dto";
+import fieldModel from "../model/field.model";
+import {equipment_status} from "@prisma/client";
 
 class EquipmentService{
     async saveEquipment(equipmentObj:EquipmentModel){
@@ -27,7 +30,22 @@ class EquipmentService{
     }
     async getAllEquipments(){
         try {
-            return await equipmentDao.findAll();
+            const equipments :EquipmentDto[] = await equipmentDao.findAll();
+            let equipmentsToReturn : EquipmentModel[] = [];
+            equipments.map((equipment :EquipmentDto) =>{
+                let staffIds = equipment.equipment_staff_details.map((staff) => staff.staff_id)
+                let fieldIds = equipment.equipment_field_details.map((field) => field.field_code);
+                equipmentsToReturn.push(new EquipmentModel(
+                    equipment.equipment_id,
+                    equipment.equipment_name || '',
+                    equipment.type || '',
+                    equipment.count || '',
+                    equipment.status as equipment_status,
+                    staffIds,
+                    fieldIds
+                ))
+            })
+            return equipmentsToReturn;
         }catch (err){
             throw err;
         }
